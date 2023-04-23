3095080a4be3c5c5d41576eb23125ff5632ed5de
Adema <aatygaeva08@gmail.com>
2023-04-23 21:29:27 +0600

    Задачи семинара 22.04

---
+++ task 2/Program.cs
@@ -0,0 +1,83 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.Write("Enter a number of the day of the week: ");
+int x = Convert.ToInt32(Console.ReadLine());
+// if (x == 1)
+// {
+//     Console.WriteLine("Monday");
+// }
+// else if (x == 2)
+// {
+//     Console.WriteLine("Tuesday");
+// }
+// else if (x == 3)
+// {
+//     Console.WriteLine("Wednesday");
+// }
+// else if (x == 4)
+// {
+//     Console.WriteLine("Thursday");
+// }
+// else if (x == 5)
+// {
+//     Console.WriteLine("Friday");
+// }
+// else if (x == 6)
+// {
+//     Console.WriteLine("Saturday");
+// }
+// else if (x == 7)
+// {
+//     Console.WriteLine("Sunday");
+// }
+// else
+// {
+//     Console.WriteLine("Not a day of the week");
+// }
+
+switch(x)
+{
+    case 1:
+    {
+        Console.WriteLine("Monday");
+        break;
+    }
+    case 2:
+    {
+        Console.WriteLine("Tuesday");
+        break;
+    }
+    case 3:
+    {
+        Console.WriteLine("Wednesday");
+        break;
+    }
+    case 4:
+    {
+        Console.WriteLine("Thursday");
+        break;
+    }
+    case 5:
+    {
+        Console.WriteLine("Friday");
+        break;
+    }
+    case 6:
+    {
+        Console.WriteLine("Saturday");
+        break;
+    }
+    case 7:
+    {
+        Console.WriteLine("Sunday");
+        break;
+    }
+    default:
+    {
+        Console.WriteLine("Not a day of the week");
+        break;
+    }
+
+}
---
+++ task 4/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.Write("Enter a number: ");
+int n = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+// if (n < 0)
+// {
+//     n *= -1;
+// }
+if (n > 99 && n < 1000) 
+{
+    Console.WriteLine($"The last digit of the number: {n % 10}");
+}
+else
+{
+    Console.WriteLine("Not a 3-digital number");
+}
